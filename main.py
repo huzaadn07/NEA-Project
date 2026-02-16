3a674ea976634453b9d3f6e8de8ed98991898d2d
huzaadn07 <huzaadn.alt@gmail.com>
2026-02-16 21:00:49 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# === Libraries ===
+
+import tkinter as tk
+import PIL as PIL                                                                               #problem line 
+import os as os
+from tkinter import ttk
+from PIL import Image, ImageTk
+
+
+# === Splash Screen ===
+
+def splash_screen(root, images\splashscreen.png):
